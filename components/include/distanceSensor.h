a11fc69fa83e7b371a369f5a11eb396b9e36fd15
Satriyo <pamungkasjati9@gmail.com>
2023-01-19 18:47:39 +0700

    add queue for echo result and trigger command, add send trigger command

--- components/include/distanceSensor.h
+++ components/include/distanceSensor.h
@@ -1,8 +1,12 @@
-#include "stdio.h"
-#include <stdint.h>
-
 #ifndef DISTANCESENSOR_H
 #define DISTANCCESENSOR_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
 typedef struct 
 {
     int distance;
@@ -10,10 +14,19 @@ typedef struct
     uint32_t trig_pin;
     uint32_t echo_pin;
     char* nameSensor;
+
+    uint64_t start_echo;
+    uint64_t stop_echo;
+    
 }Distance_Sensor;
 
-int initialize_sensor_pin(Distance_Sensor* Sensor);
+extern Queue_Type_t xEcho_TimeStamp;
+extern Queue_Type_t xTrigger_Command;
 
-int calculate_distance(Distance_Sensor* Sensor);
+esp_err_t initialize_sensor_pin( Distance_Sensor* Sensor);
+
+esp_err_t send_trigger_command(Distance_Sensor* Sensor);
+
+int calculate_distance( Distance_Sensor* Sensor);
 
 #endif
